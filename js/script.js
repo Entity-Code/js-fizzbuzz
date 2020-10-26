77a1240651b960b4aacfedfeba8fac8a906ed1a7
Mattia <mattia.tummolilllo@gmail.com>
2020-10-26 18:12:37 +0100

    Layout e logica adattata, esercizio completo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var lista = document.getElementById("lista");
 var stampa = document.getElementById("stampa");
+var back = document.getElementById("back");
 
 stampa.addEventListener("click",
 	function() {
@@ -7,22 +8,33 @@ stampa.addEventListener("click",
      for (var i = 1; i <= 100; i++) {
         // se divisibile per 3 e 5
         if( i % 3 == 0 && i % 5 == 0) {
-           document.getElementById("lista").innerHTML = "FizzBuzz";
+           document.getElementById("lista").innerHTML += "<p>FizzBuzz</p>";
            // se divisibile per 3
         } else if( i % 3 == 0 ) {
-              document.getElementById("lista").innerHTML = "Fizz";
+              document.getElementById("lista").innerHTML += "<p>Fizz</p>";
            }
            // se divisibile per 5
            else if( i % 5 == 0 ) {
-             document.getElementById("lista").innerHTML = "Buzz";
+             document.getElementById("lista").innerHTML += "<p>Buzz</p>";
            }
            // se non divisibile per entrambi
            else{
-             document.getElementById("lista").innerHTML = i;
+             document.getElementById("lista").innerHTML += "<p>" + i + "</p>" ;
            }
      }
 
      // display
-     lista.className = "dBlock";
+     lista.style.display = "block";
+     back.style.display = "block";
+     stampa.style.display = "none";
+
    }
+);
+
+// reset
+back.addEventListener("click",
+	function() {
+      lista.style.display = "none";
+      stampa.style.display = "block";
+	}
 );
