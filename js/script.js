2ebf175b50e1dc3b2481a1a5c4b7d934aaecda9d
Mattia <mattia.tummolilllo@gmail.com>
2020-10-26 16:33:29 +0100

    Layout, logica per ora adattata parzialmente a stampa in pagina.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
-// conto da 1 a 100
-for (var i = 1; i <= 100; i++) {
-   // se % per 3 e 5
-   if( i % 3 == 0 && i % 5 == 0) {
-      console.log("fizzBuzz");
-      // se divisibile per 3
-   } else if( i % 3 == 0 ) {
-         console.log("fizz");
-      }
-      // se divisibile per 5
-      else if( i % 5 == 0 ) {
-         console.log("Buzz");
-      }
-      // se non divisibile per entrambi
-      else{
-        console.log(i);
-      }
-}
+var lista = document.getElementById("lista");
+var stampa = document.getElementById("stampa");
+
+stampa.addEventListener("click",
+	function() {
+      // conto da 1 a 100
+     for (var i = 1; i <= 100; i++) {
+        // se divisibile per 3 e 5
+        if( i % 3 == 0 && i % 5 == 0) {
+           document.getElementById("lista").innerHTML = "FizzBuzz";
+           // se divisibile per 3
+        } else if( i % 3 == 0 ) {
+              document.getElementById("lista").innerHTML = "Fizz";
+           }
+           // se divisibile per 5
+           else if( i % 5 == 0 ) {
+             document.getElementById("lista").innerHTML = "Buzz";
+           }
+           // se non divisibile per entrambi
+           else{
+             document.getElementById("lista").innerHTML = i;
+           }
+     }
+
+     // display
+     lista.className = "dBlock";
+   }
+);
